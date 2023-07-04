62a26dfd8576e5e7726b5bba5db585b143d677b7
nyinyi <37504957+nyinyioo@users.noreply.github.com>
2023-07-03 17:04:55 -0700

    new changes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.write("random js code");
+// Add your JavaScript code here
+// This is just a sample code, you can add your own functionality
+
+// Example: Display an alert when the form is submitted
+var form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    alert('Form submitted!');
+});
